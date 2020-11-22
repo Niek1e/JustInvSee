2ac13f76552fb924ca8ce15232a90b76a8cc6c1f
Niek <Niek1e@users.noreply.github.com>
2020-11-22 12:47:13 +0100

    Version 8.0
    Improved stability
    Ready for Bukkit v1.13.* - v.1.16.*
    Improved usability

--- src/me/Niek1e/JustInvSee/JustInvSee.java
+++ src/me/Niek1e/JustInvSee/JustInvSee.java
@@ -1,7 +1,5 @@
 package me.Niek1e.JustInvSee;
 
-import java.io.IOException;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Effect;
@@ -9,191 +7,192 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event.Result;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.mcstats.Metrics;
-import org.mcstats.Metrics.Graph;
-
-import net.gravitydevelopment.updater.Updater;
-import net.gravitydevelopment.updater.Updater.UpdateType;
 
 public class JustInvSee extends JavaPlugin implements Listener {
-	
-	/**
-	 * @author Niek1e
-	 * @version 4.1
+
+	/*
+	 * JustInvSee v8.0, a Bukkit plugin to view a player's inventory
+	 * Copyright (C) 2020 Niek1e
+	 * 
+	 * This program is free software; you can redistribute it and/or modify it under
+	 * the terms of the GNU General Public License as published by the Free Software
+	 * Foundation; either version 2 of the License, or (at your option) any later
+	 * version.
+	 * 
+	 * This program is distributed in the hope that it will be useful, but WITHOUT
+	 * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+	 * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+	 * details.
+	 * 
+	 * You should have received a copy of the GNU General Public License along with
+	 * this program; if not, write to the Free Software Foundation, Inc., 51
+	 * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+	 * 
 	 */
 
-	public String prefix = "§f[§bJustInvSee§f] ";
-	
-	Inventory inv;
-	
-//	JustInvSee, a plugin to enhance your server.
-//	Copyright (C) 2016 Niek1e
-//
-//	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License.
-//
-//	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-//	
-//	You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-//
-//	You can contact Niek1e by email: 'prinscou@gmail.com'
-
-	@Override
 	public void onEnable() {
 		saveDefaultConfig();
-		
-		Bukkit.getServer().getPluginManager().registerEvents(this, this);
-
-		
-		try {
-		    Metrics metrics = new Metrics(this);
-
-		    Graph weaponsUsedGraph = metrics.createGraph("TEST");
-
-		    weaponsUsedGraph.addPlotter(new Metrics.Plotter("1") {
-
-		            @Override
-		            public int getValue() {
-		                    return 4; // Number of players who used a diamond sword
-		            }
-
-		    });
-
-		    weaponsUsedGraph.addPlotter(new Metrics.Plotter("2") {
-
-		            @Override
-		            public int getValue() {
-		                    return 17;
-		            }
-
-		    });
-		    
-
-		    metrics.start();
-		} catch (IOException e) {
-			this.getLogger().warning("Couldn't submit stats to Metrics!");
-		}
-
-		@SuppressWarnings("unused")
-		Updater updater = new Updater(this, 66804, getFile(), UpdateType.DEFAULT, true);
+		getServer().getPluginManager().registerEvents(this, (Plugin) this);
 	}
 
-	@Override
 	public void onDisable() {
 		saveDefaultConfig();
 	}
-	
+
 	@EventHandler
-	public void onInventoryClick(InventoryClickEvent e){
-		if(e.getInventory().getTitle() != "Armor - JustInvSee (C)"){
+	public void onInventoryClick(InventoryClickEvent e) {
+		if (e.getView().getTitle().equals("Armor - JustInvSee (C)")) {
+			e.setResult(Result.DENY);
 			return;
 		}
-		
-		e.setCancelled(true);
-		e.getWhoClicked().closeInventory();
 	}
 
+	@EventHandler
+	public void onInventoryDrag(InventoryDragEvent e) {
+		if (e.getView().getTitle().equals("Armor - JustInvSee (C)")) {
+			e.setResult(Result.DENY);
+			return;
+		}
+	}
 
-	@SuppressWarnings("deprecation")
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		Player player = (Player) sender;
+	private boolean isValidPlayer(CommandSender sender, Command command, String[] args) {
 		if (!(sender instanceof Player)) {
-			sender.sendMessage(prefix + Message.get("onlyingame", this, player));
-		} else {
+			Message exception = new Message(this, "onlyingame");
+			exception.doSend(sender);
+			return false;
+		}
+		if (!sender.hasPermission("justinvsee." + command.getName())) {
+			Message exception = new Message(this, "needop");
+			exception.doSend(sender);
+			return false;
+		}
+		if (args.length != 1) {
+			Message exception = new Message(this,
+					command.getName().substring(0, command.getName().length() - 3) + "usage");
+			exception.doSend(sender);
+			return false;
+		}
+		if (getTargetPlayer(sender, args[0]) == null) {
+			Message exception = new Message(this, "playernotonline");
+			exception.doSend(sender);
+			return false;
+		}
+		return true;
+	}
 
-			if (label.equalsIgnoreCase("inv")) {
-				if (args.length == 1) {
-					if (player.hasPermission("justinvsee.inv")) {
-						if (player.getServer().getPlayer(args[0]) != null) {
-							Player tplayer = player.getServer().getPlayer(args[0]);
-							Inventory tinv = tplayer.getInventory();
-							player.openInventory(tinv);
-							player.playEffect(tplayer.getLocation(), Effect.MOBSPAWNER_FLAMES, 0);
-							if (tplayer.isOp()) {
-								tplayer.sendMessage(prefix + Message.get("lookedinyourinv", this, player));
-							}
-						} else {
-							player.sendMessage(prefix + Message.get("playernotonline", this, player));
-						}
-					} else {
-						player.sendMessage(prefix + Message.get("needop", this, player));
-					}
-				} else {
-					player.sendMessage(prefix + Message.get("usage", this, player));
-				}
-			} else if (label.equalsIgnoreCase("enderinv")) {
-				if (args.length == 1) {
-					if (player.hasPermission("justinvsee.enderinv")) {
-						if (player.getServer().getPlayer(args[0]) != null) {
-							Player tplayer = player.getServer().getPlayer(args[0]);
-							Inventory tinv = tplayer.getEnderChest();
-							player.openInventory(tinv);
-							if (tplayer.isOp()) {
-								tplayer.sendMessage(prefix + Message.get("lookedinyourinv", this, player));
-							} else {
-							}
-						} else {
-							player.sendMessage(prefix + Message.get("playernotonline", this, player));
-						}
-					} else {
-						player.sendMessage(prefix + Message.get("needop", this, player));
-					}
-				} else {
-					player.sendMessage(prefix + Message.get("enderusage", this, player));
-				}
-			} else if (label.equalsIgnoreCase("armorinv")){
-				if (args.length == 1) {
-					if (player.hasPermission("justinvsee.armorinv")) {
-						if (player.getServer().getPlayer(args[0]) != null) {
-							Player tplayer = player.getServer().getPlayer(args[0]);
-							inv = Bukkit.getServer().createInventory(null, 9, "Armor - JustInvSee (C)");
-							ItemStack overig = createGlass(ChatColor.RED + "[]");
-							ItemStack helmet = tplayer.getEquipment().getHelmet();
-							ItemStack chestplate = tplayer.getEquipment().getChestplate();
-							ItemStack leggings = tplayer.getEquipment().getLeggings();
-							ItemStack boots = tplayer.getEquipment().getBoots();
-							ItemStack shield = tplayer.getEquipment().getItemInOffHand();
-							inv.setItem(0, overig);
-							inv.setItem(1, helmet);
-							inv.setItem(2, chestplate);
-							inv.setItem(3, leggings);
-							inv.setItem(4, boots);
-							inv.setItem(5, overig);
-							inv.setItem(6, overig);
-							inv.setItem(7, shield);
-							inv.setItem(8, overig);
-							player.openInventory(inv);
-							if (tplayer.isOp()) {
-								tplayer.sendMessage(prefix + Message.get("lookedinyourinv", this, player));
-							} else {
-							}
-						} else {
-							player.sendMessage(prefix + Message.get("playernotonline", this, player));
-						}
-					} else {
-						player.sendMessage(prefix + Message.get("needop", this, player));
-					}
-				} else {
-					player.sendMessage(prefix + Message.get("armorusage", this, player));
-				}
+	private boolean isValidCommand(CommandSender sender, Command command, String[] args) {
+
+		if (!sender.hasPermission("justinvsee.changeconfig")) {
+			Message exception = new Message(this, "needop");
+			exception.doSend(sender);
+			return false;
+		}
+
+		if (args.length != 1) {
+			Message exception = new Message(this, "justinvseeusage");
+			exception.doSend(sender);
+			return false;
+		}
+
+		return true;
+
+	}
+
+	private Player getTargetPlayer(CommandSender sender, String name) {
+		Player targetPlayer = sender.getServer().getPlayer(name);
+		return targetPlayer;
+	}
+
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+		if (command.getName().equalsIgnoreCase("justinvsee")) {
+			if (!isValidCommand(sender, command, args)) {
+				return true;
 			}
+
+			if (args[0].equalsIgnoreCase("togglemsg")) {
+				boolean setValue = toggleMessages();
+				Message exception = new Message(this, "messagetoggled", setValue);
+				exception.doSend(sender);
+				return true;
+			} else {
+				Message exception = new Message(this, "justinvseeusage");
+				exception.doSend(sender);
+				return true;
+			}
+		}
+
+		if (!isValidPlayer(sender, command, args))
+			return true;
+		Player player = (Player) sender;
+		Player targetPlayer = getTargetPlayer(sender, args[0]);
+
+		if (targetPlayer.hasPermission("justinvsee." + label) && getMessageStatus()) {
+			Message notification = new Message(this, "lookedinyourinv", targetPlayer);
+			notification.doSend(targetPlayer);
+		}
+
+		if (command.getName().equalsIgnoreCase("inv")) {
+			PlayerInventory playerInventory = targetPlayer.getInventory();
+			player.openInventory((Inventory) playerInventory);
+			player.playEffect(targetPlayer.getLocation().add(0, 1, 0), Effect.MOBSPAWNER_FLAMES, null);
+			return true;
+		}
+		if (command.getName().equalsIgnoreCase("enderinv")) {
+			Inventory targetInventory = targetPlayer.getEnderChest();
+			player.openInventory(targetInventory);
+			return true;
+		}
+		if (command.getName().equalsIgnoreCase("armorinv")) {
+			player.openInventory(getArmorInventory(targetPlayer));
+			return true;
 		}
 		return false;
 	}
-	
-	private ItemStack createGlass(String name) {
-        ItemStack i = new ItemStack(Material.STAINED_GLASS_PANE, 1);
-        ItemMeta im = i.getItemMeta();
-        im.setDisplayName(name);
-        i.setItemMeta(im);
-        return i;
-    }
-	
-	
+
+	private boolean toggleMessages() {
+		boolean value = getMessageStatus();
+		getConfig().set("opmessage", Boolean.valueOf(!value));
+		saveConfig();
+		return !value;
+	}
+
+	private boolean getMessageStatus() {
+		return getConfig().getBoolean("opmessage");
+	}
+
+	private ItemStack getGlassPane(String name) {
+		ItemStack i = new ItemStack(Material.WHITE_STAINED_GLASS_PANE, 1);
+		ItemMeta im = i.getItemMeta();
+		im.setDisplayName(name);
+		i.setItemMeta(im);
+		return i;
+	}
+
+	private Inventory getArmorInventory(Player targetPlayer) {
+		Inventory armorInventory = Bukkit.getServer().createInventory(null, 9, "Armor - JustInvSee (C)");
+		ItemStack overig = getGlassPane(ChatColor.RED + "[]");
+		armorInventory.setItem(0, overig);
+		armorInventory.setItem(1, targetPlayer.getEquipment().getHelmet());
+		armorInventory.setItem(2, targetPlayer.getEquipment().getChestplate());
+		armorInventory.setItem(3, targetPlayer.getEquipment().getLeggings());
+		armorInventory.setItem(4, targetPlayer.getEquipment().getBoots());
+		armorInventory.setItem(5, overig);
+		armorInventory.setItem(6, overig);
+		armorInventory.setItem(7, targetPlayer.getEquipment().getItemInOffHand());
+		armorInventory.setItem(8, overig);
+		return armorInventory;
+	}
+
 }
--- src/me/Niek1e/JustInvSee/Message.java
+++ src/me/Niek1e/JustInvSee/Message.java
@@ -1,34 +1,79 @@
 package me.Niek1e.JustInvSee;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class Message {
 	
-//	JustInvSee, a plugin to enhance your server.
-//	Copyright (C) 2016 Niek1e
-//
-//	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License.
-//
-//	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-//	
-//	You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-//
-//	You can contact Niek1e by email: 'prinscou@gmail.com'
-	
-	/**
-	 * @author Niek1e
-	 * @version 4.1
+	/*
+	 * JustInvSee v8.0, a Bukkit plugin to view a player's inventory
+	 * Copyright (C) 2020 Niek1e
+	 * 
+	 * This program is free software; you can redistribute it and/or modify it under
+	 * the terms of the GNU General Public License as published by the Free Software
+	 * Foundation; either version 2 of the License, or (at your option) any later
+	 * version.
+	 * 
+	 * This program is distributed in the hope that it will be useful, but WITHOUT
+	 * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+	 * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+	 * details.
+	 * 
+	 * You should have received a copy of the GNU General Public License along with
+	 * this program; if not, write to the Free Software Foundation, Inc., 51
+	 * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+	 * 
 	 */
-	
-	
 
-	public static String get(String message, JustInvSee main, Player player) {
-		String msg = "";
-		if(message == "lookedinyourinv")
-			msg = ChatColor.GOLD + player.getName() + " " + ChatColor.WHITE + main.getConfig().getString("lang." + main.getConfig().getString("language") + "." + "lookedinyourinv");
-		else
-			msg = ChatColor.RED + main.getConfig().getString("lang." + main.getConfig().getString("language") + "." + message);
-		return msg;
+	protected final String PREFIX = "[JustInvSee]";
+
+	protected JustInvSee main;
+	protected String content;
+
+	public Message(JustInvSee main, String content) {
+		this.main = main;
+		this.content = constructMessage(content);
+	}
+
+	public Message(JustInvSee main, String content, Player target) {
+		this.main = main;
+		this.content = constructMessage(content, target);
+	}
+
+	public Message(JustInvSee main, String content, boolean newValue) {
+		this.main = main;
+		this.content = constructMessage(content, newValue);
+	}
+
+	private String constructMessage(String content) {
+		String text = this.main.getConfig()
+				.getString("lang." + this.main.getConfig().getString("language") + "." + content);
+		String message = ChatColor.WHITE + "[JustInvSee]" + " " + ChatColor.RED + text;
+		return message;
+	}
+
+	private String constructMessage(String content, Player target) {
+		String text = this.main.getConfig()
+				.getString("lang." + this.main.getConfig().getString("language") + "." + content);
+		String message = ChatColor.WHITE + "[JustInvSee]" + " " + ChatColor.GOLD + target.getName() + " "
+				+ ChatColor.WHITE + text;
+		return message;
+	}
+
+	private String constructMessage(String content, boolean newValue) {
+		String text = this.main.getConfig()
+				.getString("lang." + this.main.getConfig().getString("language") + "." + content);
+		String message = ChatColor.WHITE + "[JustInvSee]" + " " + ChatColor.WHITE + text + " "
+				+ String.valueOf(newValue);
+		return message;
+	}
+
+	public void doSend(Player receiver) {
+		receiver.sendMessage(this.content);
+	}
+
+	public void doSend(CommandSender receiver) {
+		receiver.sendMessage(this.content);
 	}
 }
